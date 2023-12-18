6d2ac8091def9f062b92306dc238103c8536cd18
Tomasz Zdanowicz <tom.zdan@protonmail.com>
2023-12-18 15:12:24 +0000

    nav update

--- js/main.js
+++ js/main.js
@@ -1,10 +1,50 @@
 const navBtn = document.querySelector('.hamburger')
 const navMobile = document.querySelector('.nav__mobile')
+const navItems = document.querySelectorAll('.nav__item')
 
+const navDesktop = document.querySelector('.nav__desktop')
+const logo = document.querySelector('.header__heading--logo')
 
-const showNav = () => {
+const handleNav = () => {
     navMobile.classList.toggle('nav__mobile--active')
     navBtn.classList.toggle('is-active');
+    navItems.forEach(item => {
+		item.addEventListener('click', () => {
+			navMobile.classList.remove('nav__mobile--active')
+            navBtn.classList.remove('is-active');
+		})})
 }
 
-navBtn.addEventListener('click', showNav)
+const hideNav = () => {
+    if (window.scrollY >=100) {
+        navDesktop.style.height = '60px'
+        navDesktop.style.lineHeight = '60px'
+        navDesktop.style.fontSize = '1.2rem'
+        logo.style.scale = 0.8
+        logo.style.top = '5px'
+    } 
+    else if (window.scrollY >=200){
+navDesktop.style.display = 'none'
+    }
+    else {
+        navDesktop.style.height = '80px'
+        navDesktop.style.lineHeight = '80px'
+        navDesktop.style.fontSize = '1.5rem'
+        logo.style.scale = 1
+        logo.style.top = '15px'
+        navDesktop.style.display = 'flex'
+    }
+}
+
+// function addShadow() {
+//     if (window.scrollY >= 100) {
+//         nav.classList.add('shadow-bg');
+//     } else {
+//         nav.classList.remove('shadow-bg');
+//     }
+// }
+
+
+navBtn.addEventListener('click', handleNav)
+
+window.addEventListener('scroll', hideNav)
