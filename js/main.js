b3c3beab421ea9e17c354585fdf685c7c2453100
Tomasz Zdanowicz <tom.zdan@protonmail.com>
2023-12-27 14:15:51 +0000

    update contact form

--- js/main.js
+++ js/main.js
@@ -13,6 +13,15 @@ const analysisPopup = document.querySelector('.offer__about-offer')
 
 const footerYear = document.querySelector('.footer__year')
 
+const nameInput = document.querySelector('#name')
+const emailInput = document.querySelector('#email')
+const msgInput = document.querySelector('#msg')
+const sendBtn = document.querySelector('.send-btn')
+const errorEmail = document.querySelector('.error-email');
+const errorName = document.querySelector('.error-name');
+const errorMsg = document.querySelector('.error-msg');
+
+
 const handleNav = () => {
     navMobile.classList.toggle('nav__list--active')
     navBtn.classList.toggle('is-active');
@@ -28,35 +37,6 @@ const handleNav = () => {
 		})})
 }
 
-// const hideNav = () => {
-//     if (window.scrollY >=100 && window.innerWidth>992) {
-//         navDesktop.style.height = '60px'
-//         navDesktop.style.lineHeight = '60px'
-//         navDesktop.style.fontSize = '1.2rem'
-//         logo.style.scale = 0.8
-//         logo.style.top = '5px'
-//     } 
-//     else {
-//         navDesktop.style.height = '80px'
-//         navDesktop.style.lineHeight = '80px'
-//         navDesktop.style.fontSize = '1.5rem'
-//         logo.style.scale = 1
-//         logo.style.top = '15px'
-//     }
-// }
-
-// const handleObserver = () => {
-// 	const currentSection = window.scrollY
-
-// 	allSections.forEach(section => {
-// 		if  (section.classList.contains('white-section') && section.offsetTop <= currentSection + 60) {
-// 			navBtnBars.classList.add('black-bars-color')
-// 			} else if (!section.classList.contains('white-section') && section.offsetTop <= currentSection + 60) {
-// 				navBtnBars.classList.remove('black-bars-color')
-// 			}
-// 	})
-// }
-
 const handleAnalysisShow = () => {
     analysisPopup.classList.add('offer__about-offer--active')
 }
@@ -69,11 +49,55 @@ const handleCurrentYear = () => {
 	footerYear.innerText = year;
 }
 
+const checkName = () => {
+    if(nameInput.value){
+        errorName.style.visibility = 'hidden'
+    } else {
+        errorName.style.visibility = 'visible'
+    }
+}
+
+const checkMail = () => {
+	const re =
+		/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	if (re.test(emailInput.value)) {
+        errorEmail.style.visibility = 'hidden'
+	} else if (!emailInput.value){
+        errorEmail.textContent = 'Podaj e-mail...'
+		errorEmail.style.visibility = 'visible'
+	} else {
+        errorEmail.textContent = 'E-mail niepoprawny...'
+        errorEmail.style.visibility = 'visible'
+    }
+};
+
+const checkMsg = () => {
+    if(msgInput.value){
+        errorMsg.style.visibility = 'hidden'
+    } else {
+        errorMsg.style.visibility = 'visible'
+    }
+}
+
+const sendMsg = () => {
+    if(nameInput.value && emailInput.value && msgInput.value){
+        sendBtn.setAttribute('type', 'submit')
+        nameInput.value = ''
+        emailInput.value = ''
+        msgInput.value = ''
+    }
+}
+
+
 handleCurrentYear()
 
 navBtn.addEventListener('click', handleNav)
 analysisOpenBtn.addEventListener('click', handleAnalysisShow)
 analysisCloseBtn.addEventListener('click', handleAnalysisHide)
-
-// window.addEventListener('scroll', hideNav)
-// window.addEventListener('scroll', handleObserver)
+sendBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    checkName();
+    checkMail();
+    checkMsg();
+    sendMsg()
+})
